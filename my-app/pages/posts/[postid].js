373ec4878b745983491ee803ed09fc2fe013b2ec
Viraj Jadhav <viraj28j@gmail.com>
2021-11-09 13:23:03 +0530

    tut23 gsPaths

--- my-app/pages/posts/[postid].js
+++ my-app/pages/posts/[postid].js
@@ -10,7 +10,19 @@ function Post({ post }) {
 
 export default Post;
 
-export async function getStaticProps({ params }) {
+export async function getStaticPaths() {
+    return {
+        props: {
+            paths: [
+                {
+                    params: { postId: '1' }
+                }
+            ]
+    }
+}
+
+export async function getStaticProps(context) {
+    const { params } = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await response.json();
 
